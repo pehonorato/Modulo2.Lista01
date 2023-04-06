d2bca536e1fa29319d7ace11ae3392f55b5a5616
pehonorato <124721745+pehonorato@users.noreply.github.com>
2023-04-06 20:48:25 -0300

    Delete exercícios directory

--- exercícios/Exercício2/exercicio02.js
+++
@@ -1,9 +0,0 @@
-console.log("Nome: Ana, Idade: 25");
-
-console.log("Nome: Pedro, Idade: 30");
-
-console.log("Nome: João, Idade: 20");
-
-console.log("Nome: Maria, Idade: 27");
-
-console.log("Nome: Luiza, Idade: 22");
